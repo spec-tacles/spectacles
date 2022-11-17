2d54a8d5a61c8c9d074bc54f59a224c7edeefd51
Sam Teeuwisse <sam.teeuwisse2@gmail.com>
2022-11-17 23:26:42 +0100

    Add env variable fallback to redis broker options

--- brokers/redis/src/options.rs
+++ brokers/redis/src/options.rs
@@ -3,10 +3,10 @@ use structopt::StructOpt;
 #[derive(Debug, StructOpt)]
 #[structopt(name = "spectacles-redis")]
 pub struct Opt {
-	#[structopt(long, short, default_value = "localhost:6379")]
+	#[structopt(long, short, default_value = "localhost:6379", env = "REDIS_ADDRESS")]
 	pub address: String,
-	#[structopt(long, short)]
+	#[structopt(long, short, env = "REDIS_GROUP")]
 	pub group: String,
-	#[structopt(long, short)]
+	#[structopt(long, short, env = "REDIS_EVENTS")]
 	pub events: Vec<String>,
 }
