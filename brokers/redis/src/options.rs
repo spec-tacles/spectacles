aa958da25fe3ec1693dc73f1116de34da44b1103
Sammy <45788465+SammyWhamy@users.noreply.github.com>
2022-11-20 08:19:09 +0100

    Use config-rs instead of structopt for redis-broker (#3)
    * Add env variable fallback to redis broker options
    
    * Use config-rs instead of structopt for redis-broker

--- brokers/redis/src/options.rs
+++ brokers/redis/src/options.rs
@@ -1,12 +1,8 @@
-use structopt::StructOpt;
+use serde::{Deserialize, Serialize};
 
-#[derive(Debug, StructOpt)]
-#[structopt(name = "spectacles-redis")]
+#[derive(Debug, Serialize, Deserialize)]
 pub struct Opt {
-	#[structopt(long, short, default_value = "localhost:6379")]
 	pub address: String,
-	#[structopt(long, short)]
 	pub group: String,
-	#[structopt(long, short)]
 	pub events: Vec<String>,
 }
